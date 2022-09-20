1c7e309a6e0a023f49f9ada361378dfde540bdf1
로펀 <lopun@lopeon-ui-MacBookPro.local>
2022-09-20 23:04:41 +0900

    feat: 2018_1.clj added

--- clojure/src/aoc2018_1.clj
+++ clojure/src/aoc2018_1.clj
@@ -10,3 +10,43 @@
 ;; 예) +3, +3, +4, -2, -4 는 10이 처음으로 두번 나오는 숫자임.
 ;; 0 -> 3 (+3) -> 6 (+3) -> 10(+4) -> 8(-2) -> 4(-4) -> 7(+3) -> 10(+3) -> ...
 
+;; +1, -200, ...
+
+;; (defn apply_arithmetic_expr [prev_num expr]
+;;   (cond (= (get expr 0) \+) (+ prev_num (Integer/parseInt (subs expr 1)))
+;;         (= (get expr 0) \-) (- prev_num (Integer/parseInt (subs expr 1)))
+;;         (= (get expr 0) \/) (/ prev_num (Integer/parseInt (subs expr 1)))
+;;         (= (get expr 0) \*) (* prev_num (Integer/parseInt (subs expr 1)))))
+
+(defn read_file_as_list []
+  (clojure.string/split (slurp "resources/aoc2018_1.txt") #"\n"))
+
+(defn convert_str_list_to_int_list [str_list]
+  (map (fn [x] (Integer/parseInt x)) str_list))
+
+(comment 
+    (+ 1 1)
+    (read_file_as_list)
+    (convert_str_list_to_int_list (read_file_as_list))
+    (reduce + (convert_str_list_to_int_list (read_file_as_list)))
+  )
+
+(defn main []
+   (reduce + (convert_str_list_to_int_list (read_file_as_list))))
+
+
+;; (defn f [y]
+;; (let [a (g x)] 
+;;    ... )
+;;  const a = g(x);
+
+;;  (h a)
+;;  ()
+;;  ()
+;;  ()) <-- return
+;; 1. ["+1" "+1" "-1" ...] (O)
+;; 2. [1 1 -1 100 -5 ...]
+;; 3. ... sum
+
+
+;; PPAP : Parse Process Aggregate Print
