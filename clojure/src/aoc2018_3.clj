b9de66e0969d6880663c0dc8b99f13da05bf0dc0
로펀 <lopun@lopeon-ui-MacBookPro.local>
2022-09-22 14:54:23 +0900

    2018_3 solution added

--- clojure/src/aoc2018_3.clj
+++ clojure/src/aoc2018_3.clj
@@ -1,7 +1,8 @@
 (ns aoc2018_3
   (:require [clojure.set :as set])
   (:require [clojure.string :as string])
-  (:require [clojure.math.combinatorics :as combo]))
+  (:require [clojure.math.combinatorics :as combo])
+  (:require [clojure.core.reducers :as reducers]))
 
 (defn read-file-as-list "파일을 라인별로 분리된 리스트로 리턴해주는 함수입니다." [input-src]
   (clojure.string/split (slurp input-src) #"\n"))
@@ -30,7 +31,7 @@
 
 (defn get-cartesian-product [{id :id x :x y :y width :width height :height}]
   {:id id
-    :cart (combo/cartesian-product (into [] (range x (+ x width))) (into [] (range y (+ y height))))})
+   :cart (combo/cartesian-product (into [] (range x (+ x width))) (into [] (range y (+ y height))))})
 
 (defn parse-and-get-coordinate [line]
   (let [id (Integer/parseInt (get (string/split (get (string/split line #"#") 1) #" @ ") 0))
@@ -39,20 +40,79 @@
         width (Integer/parseInt (get (string/split (get (string/split line #": ") 1) #"x") 0))
         height (Integer/parseInt (get (string/split line #"x") 1))]
     {:id id :x x :y y :width width :height height}))
-    
-(defn cartesian-product-intersection [[{id1 :id cart1 :cart} {id2 :id cart2 :cart}]]
-  {:id1 id1 :id2 id2 :cart-intersection (set/intersection (into #{} cart1) (into #{} cart2))})
+
+;; (defn cartesian-product-intersection [[{id1 :id cart1 :cart} {id2 :id cart2 :cart}]]
+;;   {:ids [id1 id2] :cart-intersection (set/intersection (into #{} cart1) (into #{} cart2))})
+
+;; 동작은 하지만 큰 수에 대해서 동작하지 않는 로직
+;; (defn part1-solution []
+;;   (->> (read-file-as-list "resources/aoc2018_3.txt")
+;;        (map parse-and-get-coordinate)
+;;        (map get-cartesian-product)
+;;        (#(combo/combinations % 2))
+;;        (map cartesian-product-intersection)
+;;        (map #(get % :cart-intersection))
+;;        (reduce set/union)
+;;        (count)))
+
+
+(defn append-id-to-map-on-matching-coordinate [id map-value coordinate]
+  (let [prev-ids (get map-value coordinate)]
+    (if prev-ids
+      (assoc map-value coordinate (conj prev-ids id))
+      (assoc map-value coordinate #{id}))))
+
+(defn get-id-list-map-on-matching-coordinate [cart-list]
+  (loop [{id :id current-cart :cart} (first cart-list)
+         rest-cart-list (rest cart-list)
+         coordinate-map {}]
+    (let [new-coordinate-map (reduce #(append-id-to-map-on-matching-coordinate id %1 %2) coordinate-map current-cart)]
+      (if (empty? rest-cart-list)
+        new-coordinate-map
+        (recur (first rest-cart-list) (rest rest-cart-list) new-coordinate-map)))))
 
 (defn part1-solution []
-  (->> (read-file-as-list "resources/aoc2018_3_1.txt")
-       (map parse-and-get-coordinate)
-       (map get-cartesian-product)
-       (#(combo/combinations % 2))
-       (map cartesian-product-intersection)
-       (map #(get % :cart-intersection))
-       (reduce set/union)
-       (count)))
+  (let [cart-list (->> (read-file-as-list "resources/aoc2018_3.txt")
+                       (map parse-and-get-coordinate)
+                       (map get-cartesian-product))]
+    (->> cart-list
+         (get-id-list-map-on-matching-coordinate)
+         (vals)
+         (filter #(> (count %) 1))
+         (count))))
 
+;; 파트 2
+;; 입력대로 모든 격자를 채우고 나면, 정확히 한 ID에 해당하는 영역이 다른 어떤 영역과도 겹치지 않음
+;; 위의 예시에서는 ID 3 이 ID 1, 2와 겹치지 않음. 3을 출력.
+;; 겹치지 않는 영역을 가진 ID를 출력하시오. (문제에서 답이 하나만 나옴을 보장함)
+
+;; 동작은 하지만 큰 수에 대해서 동작하지 않는 로직
+;; (defn part2-solution []
+;;   (->> (read-file-as-list "resources/aoc2018_3.txt")
+;;        (map parse-and-get-coordinate)
+;;        (map get-cartesian-product)
+;;        (#(combo/combinations % 2))
+;;        (map cartesian-product-intersection)
+;;        (filter #(empty? (get % :cart-intersection)))
+;;        (map #(get % :ids))
+;;        (map #(into #{} %))
+;;        (reduce set/intersection)
+;;        ))
+
+(defn part2-solution []
+  (let [cart-list (->> (read-file-as-list "resources/aoc2018_3.txt")
+                       (map parse-and-get-coordinate)
+                       (map get-cartesian-product))
+        id-set (->> (read-file-as-list "resources/aoc2018_3.txt")
+                    (map parse-and-get-coordinate)
+                    (map #(get % :id))
+                    (into #{}))]
+    (->> cart-list
+         (get-id-list-map-on-matching-coordinate)
+         (vals)
+         (filter #(> (count %) 1))
+         (reduce set/union)
+         (set/difference id-set))))
 
 (comment
   (into #{} [1 2 3])
@@ -60,12 +120,11 @@
   (get-cartesian-product {:x 1 :y 3 :width 4 :height 4})
   (parse-and-get-coordinate "#100 @ 1,3: 4x4")
   (part1-solution)
+  (part2-solution)
+  (let [some-map {}]
+    (assoc some-map :a 1)
+    (assoc some-map :b 1))
   (combo/permutations [1 1 2])
+  (append-id-to-map-on-matching-coordinate 1 {'(2 3) #{3}} '(2 3))
   (set/intersection (into #{} [{:x 1 :y 2} {:x 1 :y 3}]) (into #{} [{:x 1 :y 2} {:x 2 :y 4}])))
-
-
-
-;; 파트 2
-;; 입력대로 모든 격자를 채우고 나면, 정확히 한 ID에 해당하는 영역이 다른 어떤 영역과도 겹치지 않음
-;; 위의 예시에서는 ID 3 이 ID 1, 2와 겹치지 않음. 3을 출력.
-;; 겹치지 않는 영역을 가진 ID를 출력하시오. (문제에서 답이 하나만 나옴을 보장함)
+  
