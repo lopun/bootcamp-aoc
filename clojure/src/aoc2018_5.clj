8d30b8487c58ff27faf058ba2faced4dd2cf56c8
로펀 <lopun@lopeon-ui-MacBookPro.local>
2022-09-26 15:59:05 +0900

    feat: doc 추가

--- clojure/src/aoc2018_5.clj
+++ clojure/src/aoc2018_5.clj
@@ -21,33 +21,79 @@
 ;; 같은 문자열에서 c/C를 없애고 모두 반응시키면 daDA가 남고 길이가 4이므로 4가 가장 짧은 길이가 됨.
 
 (defn upper-case?
+  "
+  문자가 대문자인지 확인해주는 함수입니다.
+  ex1)
+  input: A
+  output: true
+  ex2)
+  input: a
+  output: false 
+  "
   [s]
   (= s (string/upper-case s)))
 
 (defn lower-case?
+  "
+  문자가 소문자인지 확인해주는 함수입니다.
+  ex1)
+  input: A
+  output: false
+  ex2)
+  input: a
+  output: true
+  "
   [s]
   (= s (string/lower-case s)))
 
 (defn last-string
+  "
+  마지막 문자를 가져와주는 함수입니다. Character이 아닌 String 타입으로 가져옵니다.
+  ex)
+  input: abcde
+  output: e
+  "
   [s]
   (str (last s)))
- 
-(defn remove-last [str]
+
+(defn remove-last
+  "
+  마지막 문자가 제거된 문자열을 리턴하는 함수입니다.
+  ex)
+  input: abcde
+  output: abcd
+  "
+  [str]
   (.substring (java.lang.String. str) 0 (- (count str) 1)))
 
-(defn react?
+(defn can-react?
+  "
+  현재 polymer와 새로 들어온 unit이 반응할 수 있는지를 확인해주는 함수입니다.
+  ex1) d와 D가 반응
+  input: abcd D
+  output: true
+  ex2) d와 E는 반응불가
+  input: abcd E
+  output: false
+  "
   [currenty-polymer next-unit]
   (let [compare-operator (if (upper-case? next-unit) string/lower-case string/upper-case)]
     (= (last-string currenty-polymer) (compare-operator next-unit))))
 
 (defn let-polymer-react
+  "
+  polymer를 반응시키는 함수입니다. 반응할 수 있는 모든 unit은 제거됩니다.
+  ex) dabAcCaCBAcCcaDA => dabCBAcaDA
+  input: dabAcCaCBAcCcaDA
+  output: dabCBAcaDA
+  "
   [polymer]
   (->> (string/split polymer #"")
        (reduce (fn [currenty-polymer next-unit]
-                 (if (react? currenty-polymer next-unit)
+                 (if (can-react? currenty-polymer next-unit)
                    (remove-last currenty-polymer)
                    (str currenty-polymer next-unit))))))
-  
+
 (defn part1-solution
   []
   (->> (read-file-as-string "resources/aoc2018_5.txt")
@@ -55,8 +101,26 @@
        count))
 
 (defn get-all-unit
+  "
+  가능한 모든 unit(a-z)을 리턴하는 함수입니다. 해당 함수에서는 소문자만 리턴합니다.
+  ex)
+  input:
+  output: (\a \b \c ... \z)
+  "
   []
   (map char (range (int \a) (inc (int \z)))))
+
+(defn remove-unit-from-polymer
+  "
+  polymer에서 unit에 해당하는 모든 문자를 제거하는 함수입니다.
+  ex)
+  input: \a abcdABCD
+  output: bcdBCD
+  "
+  [unit polymer]
+  (-> polymer
+      (string/replace (string/lower-case unit) "")
+      (string/replace (string/upper-case unit) "")))
 
 
 (comment
@@ -66,5 +130,7 @@
   (part1-solution)
   (remove-last "abc")
   (let-polymer-react  "abcCdDBe")
+  (remove-unit-from-polymer \a "abAc")
   (get-all-unit)
   (last "abcde"))
+  
