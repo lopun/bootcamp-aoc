8324884db2769724e53bd31ea675b2c0bd5395fb
로펀 <lopun@lopeon-ui-MacBookPro.local>
2022-10-05 15:50:33 +0900

    feat: 2018_7 solved & 2020_8 refactored

--- clojure/src/aoc2018_7.clj
+++ clojure/src/aoc2018_7.clj
@@ -11,24 +11,59 @@
        rest
        ((fn [[from to]] {:from from :to to}))))
 
-(defn group-steps
+(defn instructions
   [steps]
   (->> steps
-       (group-by :from)
-       (fmap #(map :to %))))
+       (group-by :to)
+       (fmap #(map :from %))))
 
-(defn get-start-letter
-  [step-group]
-  (let [all-letters (into #{} (concat (keys step-group) (flatten (vals step-group))))
-        val-letters (into #{} (flatten (vals step-group)))]
-    (first (set/difference all-letters val-letters))))
+(defn all-steps
+  [steps]
+  (into #{} (concat (map :to steps) (map :from steps))))
+
+(defn get-start-steps
+  [{:keys [all-steps instructions]}]
+  (sort (set/difference all-steps
+                        (into #{} (keys instructions)))))
+
+(defn to-manual
+  [steps]
+  {:instructions (instructions steps) :all-steps (all-steps steps)})
+
+(defn renew-available
+  [available answer {:keys [all-steps instructions] :as _manual}]
+  (let [completed-set (into #{} answer)
+        not-completed-set (set/difference all-steps completed-set)]
+    (->> not-completed-set
+         (filter #(empty? (set/difference (into #{} (instructions %)) completed-set)))
+         (concat available)
+         distinct
+         sort)))
+
+(defn find-steps-for-one-elf [manual]
+  (loop [available (get-start-steps manual)
+         answer []]
+    (if (empty? available)
+      answer
+      (let [result (conj answer (first available))]
+        (recur (renew-available (rest available) result manual) result)))))
 
 (defn part1-solution
   []
   (->> (read-file-as-list "aoc2018_7.txt")
        (map parse-step)
-       group-steps
-       get-start-letter))
+       to-manual
+       find-steps-for-one-elf
+       (apply str)))
+
+(defn find-steps-for-elves-with-sec
+  [manual elves-count secs]
+  (loop [available (get-start-steps manual)
+         answer []
+         count 0
+         elves []]
+    (if (and (empty? available) (empty? elves))
+      count)))
 
 
 (comment
